f0ea63be80297b6e2a5aaa45fced23b926a4c57e
romanzavarzin <romalbj@mail.ru>
2024-04-29 19:32:42 +0300

    active page horizontal list

--- better_site/main/static/main/js/script.js
+++ better_site/main/static/main/js/script.js
@@ -14,6 +14,30 @@ console.log("Ширина экрана: " + width + "px");
 console.log("Высота экрана: " + height + "px");
 
 
+
+let scrollContainer = document.querySelector('.rec_list');
+let arrow_backward = document.getElementById('arrow_backward');
+let arrow_forward = document.getElementById('arrow_forward');
+
+scrollContainer.addEventListener('wheel', (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = 'auto';
+
+});
+
+arrow_forward.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft += 500;
+});
+
+arrow_backward.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = 'smooth';
+    scrollContainer.scrollLeft -= 500;
+});
+
+
+
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
