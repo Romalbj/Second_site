1dd4867af4b52233333194bed444b1ad333e1405
romanzavarzin <romalbj@mail.ru>
2024-06-18 19:10:10 +0300

    some changes

--- better_site/accounts/views.py
+++ better_site/accounts/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-# from main.models import Articles
+from .forms import CreateUserForm
 
 def sign_up(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("better_site:home")
+            return redirect("home")
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     return render(request, 'accounts/sign_up.html', {'form': form})
-    # return HttpResponse('hi')
+
 
