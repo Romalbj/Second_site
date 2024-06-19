3c38c74ee5fb6862b8cef1b54254521891f1b43c
romanzavarzin <romalbj@mail.ru>
2024-06-19 20:55:22 +0300

    registration page normal look

--- better_site/accounts/forms.py
+++ better_site/accounts/forms.py
@@ -3,6 +3,22 @@ from django import forms
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
+
+    username = forms.CharField(max_length=100,
+                           widget= forms.TextInput
+                           (attrs={'placeholder':' Имя пользователя'}))
+
+    email = forms.CharField(max_length=100,
+                           widget= forms.TextInput
+                           (attrs={'placeholder':'Почта'}))
+
+    password1 = forms.CharField(max_length=100,
+                           widget= forms.TextInput
+                           (attrs={'placeholder':'Пароль'}))
+
+    password2 = forms.CharField(max_length=100,
+                           widget= forms.TextInput
+                           (attrs={'placeholder':'Повторите пароль'}))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
