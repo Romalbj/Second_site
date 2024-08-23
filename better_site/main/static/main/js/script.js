5c8b618957039b4a9031fed554748fc62858f680
romanzavarzin <romalbj@mail.ru>
2024-08-23 18:48:01 +0300

    like button hover animation

--- better_site/main/static/main/js/script.js
+++ better_site/main/static/main/js/script.js
@@ -201,3 +201,19 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+//for loke animation
+
+const heartIcon = document.querySelector(".like-button .heart-icon");
+const likesAmountLabel = document.querySelector(".like-button .likes-amount");
+
+
+heartIcon.addEventListener("click", () => {
+  heartIcon.classList.toggle("liked");
+  if (heartIcon.classList.contains("liked")) {
+    likesAmountLabel++;
+  } else {
+    likesAmountLabel--;
+  }
+
+});
