b16680116a672adf5c71694da105125c18a4d69e
romanzavarzin <romalbj@mail.ru>
2024-08-23 21:55:13 +0300

    unlike func

--- better_site/article_detail/views.py
+++ better_site/article_detail/views.py
@@ -20,9 +20,24 @@ def article_detail(request, id, category):
     article = Articles.objects.get(id=id)
     total_likes = article.likes.count()
     user = request.user
+
+    liked = False
+    if article.likes.filter(id=request.user.id).exists():
+        liked = True
+
     return render(request, 'article_detail/article_detail.html', {'article': article, 'id': id, 'category': category, 'form': CommentForm,
-                                                                  'user': user, 'total_likes': total_likes})
+                                                                  'user': user, 'total_likes': total_likes, 'liked': liked, })
 def like_article(request, id, category):
     post = get_object_or_404(Articles, id=request.POST.get('article_id'))
-    post.likes.add(request.user)
+
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
+
+
     return redirect(f'http://127.0.0.1:8000/article/{category}/{id}')
