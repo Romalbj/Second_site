97b31dec8f252b5a9e0245ce18593a4a205b0e5c
romanzavarzin <romalbj@mail.ru>
2025-01-23 11:50:10 +0300

    postgres instead of sqllite

--- better_site/better_site/settings.py
+++ better_site/better_site/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'better_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'test',
+        'PASSWORD': 'test password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
